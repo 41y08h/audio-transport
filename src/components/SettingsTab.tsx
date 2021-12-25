86cf32f49eb25cb0c0fc751e0be51756c5865f07
Piyush <piyush41y08h@gmail.com>
2021-12-25 09:53:52 +0530

    add active prop in settings tab

--- src/components/SettingsTab.tsx
+++ src/components/SettingsTab.tsx
@@ -2,17 +2,25 @@ import classNames from "classnames";
 import { FC } from "react";
 import { Link, LinkProps } from "react-router-dom";
 
-const SettingsTab: FC<LinkProps & React.RefAttributes<HTMLAnchorElement>> = ({
+type LinkPropsType = LinkProps & React.RefAttributes<HTMLAnchorElement>;
+
+interface Props extends LinkPropsType {
+  active?: boolean;
+}
+
+const SettingsTab: FC<Props> = ({
   children,
   className,
+  active = false,
   ...props
 }) => {
   return (
     <Link
       {...props}
       className={classNames(
-        className,
-        "w-full text-center py-3 bg-gray-200/20 rounded-t-md text-white flex items-center justify-center space-x-2"
+        "w-full text-center py-3 rounded-t-md text-white flex items-center justify-center space-x-2",
+        active ? "bg-gray-200/30" : "bg-gray-200/10",
+        className
       )}
     >
       {children}
